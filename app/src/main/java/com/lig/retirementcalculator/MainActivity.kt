c83b9c9635508447a06764e1f6cd7fd0323df3f3
guopengli <liguopengbenli@gmail.com>
2021-08-20 16:50:54 +0200

    set basic test env

--- app/src/main/java/com/lig/retirementcalculator/MainActivity.kt
+++ app/src/main/java/com/lig/retirementcalculator/MainActivity.kt
@@ -2,6 +2,8 @@ package com.lig.retirementcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
@@ -10,10 +12,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
+        AppCenter.setLogLevel(Log.VERBOSE)
         AppCenter.start(application, "15666653-f39c-403b-a5b3-5539438a853e", Analytics::class.java, Crashes::class.java)
-
-
+        val btn  = findViewById<Button>(R.id.calculateButton)
+        btn.setOnClickListener {
+            //Crashes.generateTestCrash()
+            //throw Exception("Something wrong happened")
+        }
     }
 }
