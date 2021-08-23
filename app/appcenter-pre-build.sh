824db9adf57405b3705336317fe1ea5ae1ae435b
guopengli <liguopengbenli@gmail.com>
2021-08-23 16:44:03 +0200

    update pre build script for app center 2

--- app/appcenter-pre-build.sh
+++ app/appcenter-pre-build.sh
@@ -1,5 +1,5 @@
 echo "pre build script start"
-if [ -z "$VERSION_NAME"] 
+if [ -z "$VERSION_NAME" ] 
 then 
     echo "You need to define the VERSION_NAME variable in App Center"
     exit
@@ -7,7 +7,7 @@ fi
 
 ANDROID_GRADLE_FILE=$APPCENTER_SOURCE_DIRECTORY/app/build.gradle
 
-if [ -e "$ANDROID_GRADLE_FILE"]
+if [ -e "$ANDROID_GRADLE_FILE" ]
 then 
     echo "Updating version name to $VERSION_NAME ($APPCENTER_BUILD_ID) in build.gradle"
     sed -i '' 's/versionName "[0-9]*"/versionName "'$VERSION_NAME'"/' $ANDROID_GRADLE_FILE
